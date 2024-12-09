69fb744b91c5215b1d3e2518a2f9d233b1a2d689
samotis <samotis@wisc.edu>
2024-12-09 20:29:21 +0000

    delete old server

--- server/server.js
+++ server/server.js
@@ -5,7 +5,8 @@ const { Sequelize } = require('sequelize');
 const path = require('path');
 const cors = require('cors');
 
-//Configuration /Middleware
+
+// //Configuration /Middleware
 require('dotenv').config();
 app.use(cors());
 app.use(express.json());
@@ -16,12 +17,13 @@ app.use(express.static(path.join(__dirname, '../build')));
 const trailsController = require('./controllers/trails_controller');
 app.use('/api/trails', trailsController);
 
+
+
 // LISTEN
-//const PORT = process.env.PORT || 5001;
 app.listen(8000, () => {
     console.log(`Server is running on port 8000`);
 })
 
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../build/index.html'));
-// })
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build/index.html'));
+})
